ea077aa432bb3cd93f4518b2628c13068915c348
zurimokato <noj2304@gmail.com>
2019-05-29 22:17:18 -0500

    lo que hace flata

--- app/src/main/java/job/project/com/roomapp/MainActivity.java
+++ app/src/main/java/job/project/com/roomapp/MainActivity.java
@@ -1,13 +1,27 @@
 package job.project.com.roomapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buton=findViewById(R.id.reservar);
+
+        buton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
